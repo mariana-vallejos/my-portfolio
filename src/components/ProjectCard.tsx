d8a7f8649208bafa1a1b141dba138f5f2076aa4b
Mariana Vallejos <mariana.vallejos@assuresoft.tech>
2025-09-17 00:05:11 -0400

    add projects section and project card component

---
+++ src/components/ProjectCard.tsx
@@ -0,0 +1,25 @@
+import { CiLink } from "react-icons/ci";
+import type { Project } from "../assets/data/projects";
+
+const ProjectCard = ({ title, description, image, link }: Project) => {
+  return (
+    <div className="w-96 px-4 py-2 relative bg-cyan-200/40 rounded-[30px] shadow-[0px_45.90865707397461px_45.90865707397461px_0px_rgba(69,42,124,0.15)] outline-[1.53px] outline-offset-[-1.53px] outline-white/60 backdrop-blur-[10px] overflow-hidden">
+      <img src={image} alt={`${title} image`} className="h-72" />
+      <div className="flex flex-col items-center">
+      <h4 className="font-semibold text-center">{title}</h4>
+      <p>{description}</p>
+
+        <a
+          href={link}
+          target="_blank"
+          className="bg-dark3 w-24 rounded-md py-3 my-3 flex gap-1 items-center justify-center"
+        >
+          <CiLink /> See
+        </a>
+
+      </div>
+    </div>
+  );
+};
+
+export default ProjectCard;
--- src/components/ProjectsSection.tsx
+++ src/components/ProjectsSection.tsx
@@ -1,8 +1,24 @@
+import { projects } from "../assets/data/projects";
+import ProjectCard from "./ProjectCard";
 
 const ProjectsSection = () => {
   return (
-    <div className="h-96">ProjectsSection</div>
-  )
-}
+    <section className="py-20 px-10">
+      <h2 className="text-2xl font-semibold text-center">My Projects</h2>
+      <div className="pt-10 pb-5 flex gap-3">
+        {projects.map((project) => (
+          <ProjectCard
+            title={project.title}
+            description={project.description}
+            link={project.link}
+            image={project.image}
+            id={project.id}
+            key={project.id}
+          />
+        ))}
+      </div>
+    </section>
+  );
+};
 
-export default ProjectsSection
+export default ProjectsSection;
